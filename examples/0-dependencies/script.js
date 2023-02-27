9406875e628271e16cf7bf723c3d415a4cd4eeb4
luttje <2738114+luttje@users.noreply.github.com>
2023-02-27 18:19:03 +0100

    fix 0-dependencies example (pre-render frames)

--- examples/0-dependencies/script.js
+++ examples/0-dependencies/script.js
@@ -11,15 +11,31 @@ window.addEventListener('message', function (event) {
   }
 });
 
-const squares = document.querySelectorAll('div');
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-let initialHues = [90, 180, 270, 360];
-let hues = [];
+const squares = document.querySelectorAll('.colored-block');
 
 const width = 360;
 const height = 640;
 const framerate = 30;
-const frameCount = 2 * framerate; // seconds
+const frameCount = 2 * framerate; // 2 seconds
+
+// A list of functions that will be run to render each frame
+const frames = [];
+
+// We go all around the color wheel
+const hueChange = 360 / frameCount;
+
+// Generate functions that will set the background color of each square for each frame
+let hues = [null, 180, 270, 360];
+
+for (let frame = 0; frame < frameCount; frame++) {
+  frames[frame] = () => {
+    squares.forEach((square, i) => {
+      const hue = hues[i] + hueChange * frame;
+
+      square.style.backgroundColor = `hsl(${hue}, 100%, 50%)`;
+    });
+  };
+}
 
 function messageRenderer(type, data) {
   parent.postMessage({ ...data, type }, '*');
@@ -35,16 +51,8 @@ function setup() {
 }
 
 function tick(frame) {
-  if(frame === 0) {
-    hues = [...initialHues];
+  // run all frames up to the and including the current frame
+  for (let i = 0; i <= frame; i++) {
+    frames[i]();
   }
-
-  move();
-}
-
-function move() {
-  hues = hues.map(hue => (hue + 1) % 360);
-  squares.forEach((square, i) => {
-    square.style.backgroundColor = `hsl(${hues[i]}, 100%, 50%)`;
-  });
 }
