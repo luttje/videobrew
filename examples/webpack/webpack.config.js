3b254989414b98058cbe3f163eb8432cdf100279
luttje <2738114+luttje@users.noreply.github.com>
2023-03-01 21:16:57 +0100

    dont open webpack serve

--- examples/webpack/webpack.config.js
+++ examples/webpack/webpack.config.js
@@ -14,7 +14,6 @@ const config = {
         path: path.resolve(__dirname, 'dist'),
     },
     devServer: {
-        open: true,
         port: 8088,
         host: 'localhost',
         devMiddleware: {
