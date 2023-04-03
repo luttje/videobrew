8032808329217dd2dbb30ae96ab530cadaaaa2e3
luttje <2738114+luttje@users.noreply.github.com>
2023-04-03 22:05:32 +0200

    fix exports

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -4,7 +4,8 @@ import { Scene } from './scene.js';
 import { SceneBuilder } from './scene-builder.js';
 import { Video } from './video.js';
 import { VideoBuilder } from './video-builder.js';
-import { VideoAppSetup, VideoApp } from './video-app.js';
+import { VideoApp } from './interfaces/video-app.js';
+import { VideoAppSetup } from './interfaces/video-app-setup';
 
 export {
   modifyTransform,
