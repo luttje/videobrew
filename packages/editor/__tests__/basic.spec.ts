effefbbe00ec4ba93712449cd542e016aa5f155b
luttje <2738114+luttje@users.noreply.github.com>
2023-04-05 10:38:58 +0200

    simplify test config

--- packages/editor/__tests__/basic.spec.ts
+++ packages/editor/__tests__/basic.spec.ts
@@ -16,101 +16,97 @@ function normalizeArray(arr: any[]) {
 }
 
 test.describe('basic', async () => {
-  const previewPort = 8087
-  
   let server: PreviewServer
   let browser: Browser
   let page: Page
 
   test.beforeAll(async () => {
-    // TODO: Start the video app at http://localhost:8088
-    // exec(...)
-
     process.env.VIDEOBREW_VIDEO_APP_URL = 'http://localhost:8088'
-    server = await preview({ preview: { port: previewPort }})
+    server = await preview({
+      configFile: 'vite.config.ts',
+    })
     browser = await chromium.launch()
     page = await browser.newPage()
 
-    // TODO: Check for --coverage
-    await page.coverage.startJSCoverage();
+    if (process.env.COLLECT_COVERAGE) {
+      await page.coverage.startJSCoverage()
+    }
   })
 
   test.afterAll(async () => {
-    const coverage = await page.coverage.stopJSCoverage();
-    //const debug = fs.readFileSync('coverage/coverage.json', 'utf8'); // debugging
-    //const istanbulCoverage = JSON.parse(debug);
-    //const trimPath = 'C:\\Projects\\videobrew\\packages\\client\\';
-    const prefix = '.svelte-kit/output/client/';
-    const trimPath = '';
-    let istanbulCoverage: object = {};
+    if (process.env.COLLECT_COVERAGE) {
+      const coverage = await page.coverage.stopJSCoverage();
+      const prefix = '.svelte-kit/output/client/';
+      const trimPath = '';
+      let istanbulCoverage: object = {};
 
-    for (const entry of coverage) {
-      const path = entry.url.replace('http://localhost:8087/', prefix);
+      for (const entry of coverage) {
+        const path = entry.url.replace('http://localhost:8087/', prefix);
 
-      if(!path || path == prefix || !fs.existsSync(path)) continue;
+        if(!path || path == prefix || !fs.existsSync(path)) continue;
 
-      const converter = v8toIstanbul(path);
-      await converter.load();
-      converter.applyCoverage(entry.functions);
+        const converter = v8toIstanbul(path);
+        await converter.load();
+        converter.applyCoverage(entry.functions);
+        
+        const istanbul = converter.toIstanbul();
+        istanbulCoverage = { ...istanbulCoverage, ...istanbul};
+      }
+
+      if (!fs.existsSync('coverage'))
+        fs.mkdirSync('coverage');
       
-      const istanbul = converter.toIstanbul();
-      istanbulCoverage = { ...istanbulCoverage, ...istanbul};
-    }
+      fs.writeFileSync(
+        'coverage/coverage.json',
+        JSON.stringify(istanbulCoverage, null, 2)
+      );
 
-    if (!fs.existsSync('coverage'))
-      fs.mkdirSync('coverage');
-    
-    fs.writeFileSync(
-      'coverage/coverage.json',
-      JSON.stringify(istanbulCoverage, null, 2)
-    );
+      fs.writeFileSync('coverage/lcov.info', '');
 
-    fs.writeFileSync('coverage/lcov.info', '');
+      // Convert coverage to lcov and save to LCOV file
+      // TODO: Extract this to a separate npm package
+      for (const key in istanbulCoverage) {
+        const entry = istanbulCoverage[key];
+        const file = entry.path.replace(trimPath, '');
+        const lines = normalizeArray(entry.statementMap);
+        const lineHits = normalizeArray(entry.s);
+        const functions = normalizeArray(entry.fnMap);
+        const functionHits = normalizeArray(entry.f);
+        const branches = normalizeArray(entry.branchMap);
+        const branchHits = normalizeArray(entry.b);
 
-    // Convert coverage to lcov and save to LCOV file
-    for (const key in istanbulCoverage) {
-      const entry = istanbulCoverage[key];
-      const file = entry.path.replace(trimPath, '');
-      const lines = normalizeArray(entry.statementMap);
-      const lineHits = normalizeArray(entry.s);
-      const functions = normalizeArray(entry.fnMap);
-      const functionHits = normalizeArray(entry.f);
-      const branches = normalizeArray(entry.branchMap);
-      const branchHits = normalizeArray(entry.b);
-
-      const lcov = [
-        `TN:`,
-        `SF:${file}`,
-        ...functions.map((fn: any) => `FN:${fn.line},${fn.name}`),
-        `FNF:${functions.length}`,
-        `FNH:${functionHits.filter((hit: any) => hit > 0).length}`,
-        ...functions.map((fn: any, index: number) => `FNDA:${functionHits[index]},${fn.name}`),
-        ...lines.map((line: any, index: number) => {
-          return `DA:${line.start.line},${lineHits[index]}`;
-        }),
-        `LF:${lines.length}`,
-        `LH:${lines.length}`,
-        ...branches.map((branch: any, index: number) => {
-          return `BRDA:${branch.line},${index},${0},${branchHits[index]}`;
-        }),
-        `BRF:${branches.length}`,
-        `BRH:${branches.length}`,
-        `end_of_record`,
-      ].filter(Boolean).join('\n');
-    
-      fs.appendFileSync('coverage/lcov.info', lcov + '\n');
+        const lcov = [
+          `TN:`,
+          `SF:${file}`,
+          ...functions.map((fn: any) => `FN:${fn.line},${fn.name}`),
+          `FNF:${functions.length}`,
+          `FNH:${functionHits.filter((hit: any) => hit > 0).length}`,
+          ...functions.map((fn: any, index: number) => `FNDA:${functionHits[index]},${fn.name}`),
+          ...lines.map((line: any, index: number) => {
+            return `DA:${line.start.line},${lineHits[index]}`;
+          }),
+          `LF:${lines.length}`,
+          `LH:${lines.length}`,
+          ...branches.map((branch: any, index: number) => {
+            return `BRDA:${branch.line},${index},${0},${branchHits[index]}`;
+          }),
+          `BRF:${branches.length}`,
+          `BRH:${branches.length}`,
+          `end_of_record`,
+        ].filter(Boolean).join('\n');
+      
+        fs.appendFileSync('coverage/lcov.info', lcov + '\n');
+      }
     }
 
     await browser.close()
     await new Promise<void>((resolve, reject) => {
       server.httpServer.close(error => error ? reject(error) : resolve())
-
-      // Close video app
     })
   })
 
   test('should run test suite', async () => {
-    await page.goto(`http://localhost:${previewPort}`)
+    await page.goto(`/`)
     const h1 = await page.waitForSelector('h1')
     expect(await h1.innerText()).toBe('Loading...')
   })
--- packages/editor/playwright.config.ts
+++ packages/editor/playwright.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
+const editorUrl = 'http://localhost:8087';
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -21,14 +17,6 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-  },
 
   /* Configure projects for major browsers */
   projects: [
@@ -68,10 +56,17 @@ export default defineConfig({
     // },
   ],
 
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  /* Run the editor (and an example) server before starting the tests */
+  webServer: {
+    command: 'npm run dev',
+    url: editorUrl,
+    reuseExistingServer: !process.env.CI,
+  },
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: editorUrl,
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+  },
 });
