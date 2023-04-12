f2ccce1ea5d8d11710aa58eae3e9cc0c11dbc6f1
luttje <2738114+luttje@users.noreply.github.com>
2023-04-12 14:18:23 +0200

    build docs to package root

--- scripts/docs-build.mjs
+++ scripts/docs-build.mjs
@@ -8,25 +8,23 @@ const packagePath = cwd(); // script should only be run from package root
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const DISTRIBUTION_DIRECTORY = 'dist';
-
 async function main() {
   // Merge README with PACKAGE_README into dist
   fs.writeFileSync(
-    path.join(packagePath, DISTRIBUTION_DIRECTORY, 'README.md'),
+    path.join(packagePath, 'README.md'),
     (fs.readFileSync(path.join(packagePath, 'PACKAGE_README.md'), 'utf8') +
       '\n' +
       fs.readFileSync(path.join(__dirname, '..', 'README.md'), 'utf8'))
   )
   
   fs.copyFileSync(
-    path.join(packagePath, 'LICENSE'),
-    path.join(packagePath, DISTRIBUTION_DIRECTORY, 'LICENSE')
+    path.join(__dirname, '..', 'LICENSE'),
+    path.join(packagePath, 'LICENSE')
   );
 
   fs.copyFileSync(
     path.join(__dirname, '..', 'LICENSES-THIRD-PARTY'),
-    path.join(packagePath, DISTRIBUTION_DIRECTORY, 'LICENSES-THIRD-PARTY')
+    path.join(packagePath, 'LICENSES-THIRD-PARTY')
   );
 }
 
