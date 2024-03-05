f574d4d110a0689c7ca95e556d6a947721e78720
luttje <2738114+luttje@users.noreply.github.com>
2024-03-05 20:57:39 +0100

    fix config, run help smoke test

--- scripts/npm-package-test.mjs
+++ scripts/npm-package-test.mjs
@@ -1,8 +1,10 @@
-import { spawnCommand, spawnCommandAndMoveOn } from './run.mjs';
+import { run, runNpm, spawnCommand, spawnCommandAndMoveOn } from './run.mjs';
+import path from 'path';
+import fs from 'fs';
 
 async function startVerdaccio() {
   return await spawnCommandAndMoveOn(
-    'verdaccio', 
+    'verdaccio',
     ['--listen', '4000', '--config', './scripts/verdaccio-config.yaml'],
     'Verdaccio started successfully.',
     'Verdaccio'
@@ -11,39 +13,91 @@ async function startVerdaccio() {
 
 async function runLernaPublish() {
   await spawnCommand(
-    'npx', 
+    'npx',
     ['lerna', 'publish', '--canary', '--no-git-tag-version', '--no-push', '--force-publish', '--no-private', '--registry', 'http://localhost:4000', '--yes'],
     'Lerna publish ran successfully.',
     'Lerna publish'
   );
 }
 
-async function runNpmPackageTest() {
-  // await spawnCommand(
-  //   'npm',
-  //   ['run', 'npm-package-test'],
-  //   'Npm-package-test ran successfully.',
-  //   'Npm-package-test'
-  // );
-  // TODO: For CI we could just do this by installing it, but for dev we need to sandbox this in a way that does not have the package conflict on the system.
-  console.log('todo: somehow install the packages from the local registry and run the tests');
+/**
+ * Creats a mock workspace and installs the package in it.
+ * 
+ * Ideally we would install it globally, but this way we can remove the workspace after the test.
+ * Globally it could conflict on the developer's machine.
+ */
+async function installNpmPackageInMockWorkspace() {
+  const mockWorkspaceDir = path.resolve('./scripts/mock-workspace');
+
+  try {
+    fs.mkdirSync(mockWorkspaceDir, { recursive: true });
+    runNpm(`init -y`, mockWorkspaceDir);
+    runNpm(`install @videobrew/cli --registry=http://localhost:4000`, mockWorkspaceDir);
+
+    console.log('Package installed in mock workspace.');
+
+    // Return a callback to remove the workspace
+    return async () => {
+      // Without this if it's too scary to run rmdir
+      if (!mockWorkspaceDir.includes('scripts/mock-workspace') && !mockWorkspaceDir.includes('scripts\\mock-workspace')) {
+        throw new Error('Refusing to remove the workspace, as it is not in the expected location: ' + mockWorkspaceDir);
+      }
+      
+      fs.rmdirSync(mockWorkspaceDir, { recursive: true, force: true });
+      console.log('Mock workspace removed.');
+    };
+  } catch (error) {
+    console.error('Failed to install the package in mock workspace:', error);
+    throw error;
+  }
+}
+
+/**
+ * Runs the tests in the mock workspace.
+ * 
+ * @returns {boolean} True if the tests passed.
+ */
+async function runNpmTests() {
+  const mockWorkspaceDir = path.resolve('./scripts/mock-workspace');
+
+  const output = run(
+    'npx videobrew --help',
+    mockWorkspaceDir
+  );
+
+  if (!output.includes('Create videos using web technologies.')) {
+    throw new Error('The output of "npx videobrew --help" did not contain the expected string.');
+  } else {
+    console.log('The output of "npx videobrew --help" contained the expected string.');
+  }
+
+  return true;
 }
 
 async function main() {
   try {
     const killVerdaccio = await startVerdaccio();
+
+    await runLernaPublish();
+    const workspaceRemover = await installNpmPackageInMockWorkspace();
     
-    try {
-      await runLernaPublish();
-      await runNpmPackageTest();
-    } catch (error) {
-      console.error('An error occurred in runLernaPublish or runNpmPackageTest:', error);
-    }
+    console.log('Running npm integration tests...');
+    const success = runNpmTests();
+
+    await workspaceRemover();
 
     killVerdaccio();
+
+    if (!success) {
+      throw new Error('Tests failed.');
+    }
+
+    console.log('All npm integration tests passed.');
+    process.exit(0);
   } catch (error) {
     console.error('An error occurred while starting Verdaccio:', error);
+    process.exit(1);
   }
 }
 
-main().catch(error => console.error('Unhandled error in main:', error));
+main();
