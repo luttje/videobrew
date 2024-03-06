9641120681c0c04d779661c04c634a61ffb683c9
luttje <2738114+luttje@users.noreply.github.com>
2024-03-06 11:58:53 +0100

    Fix svelte component test example

--- packages/editor/src/lib/components/Setting.svelte
+++ packages/editor/src/lib/components/Setting.svelte
@@ -1,7 +1,8 @@
 <script>
-  import { createEventDispatcher } from 'svelte';
+  import { createEventDispatcher } from "svelte";
 </script>
-<div class="flex flex-row gap-4 items-center">
+
+<div class="flex flex-row gap-4 items-center" role="setting">
   <span class="text-sm italic">
     <slot />:
   </span>
